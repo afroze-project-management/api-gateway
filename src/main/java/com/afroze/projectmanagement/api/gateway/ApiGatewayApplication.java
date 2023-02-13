e1d9b82a94933f70db2065db49a98f2e58744001
afroze9 <afrozeamjad@gmail.com>
2023-02-13 18:58:51 +0500

    aws config for eureka

--- src/main/java/com/afroze/projectmanagement/api/gateway/ApiGatewayApplication.java
+++ src/main/java/com/afroze/projectmanagement/api/gateway/ApiGatewayApplication.java
@@ -1,11 +1,18 @@
 package com.afroze.projectmanagement.api.gateway;
 
+import com.netflix.appinfo.AmazonInfo;
+import com.netflix.appinfo.EurekaInstanceConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.commons.util.InetUtils;
+import org.springframework.cloud.netflix.eureka.EurekaInstanceConfigBean;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ApiGatewayApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ApiGatewayApplication.class, args);
 	}
+
+
 }
