7d5993b046833a9041870ed05a2fcb15413b5956
afroze9 <afrozeamjad@gmail.com>
2023-02-21 11:21:46 +0500

    Added EnableDiscoveryClient

--- src/main/java/com/afroze/projectmanagement/api/gateway/ApiGatewayApplication.java
+++ src/main/java/com/afroze/projectmanagement/api/gateway/ApiGatewayApplication.java
@@ -4,6 +4,7 @@ import com.netflix.appinfo.AmazonInfo;
 import com.netflix.appinfo.EurekaInstanceConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.commons.util.InetUtils;
 import org.springframework.cloud.netflix.eureka.EurekaInstanceConfigBean;
 import org.springframework.context.annotation.Bean;
@@ -12,6 +13,7 @@ import java.net.InetAddress;
 import java.net.UnknownHostException;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ApiGatewayApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ApiGatewayApplication.class, args);
